3ed09c1cd3810400d9da11db412c42d92f4233ad
juliaaleixo <julia.pereira1995@outlook.com>
2017-09-26 11:11:22 -0300

    .

--- HillClimbing/hillclimbing.py
+++ HillClimbing/hillclimbing.py
@@ -21,7 +21,7 @@ def selectTSP(cwd):
     return fullpathfile
 
 
-def readTSP(selectedTSP):
+def readTSP(selectedTSP): #le o arquivo do TSP e salva em uma matriz de distancias
 
     file = open(selectedTSP, 'r')
     lines = []
@@ -59,7 +59,7 @@ def readTSP(selectedTSP):
                 for word in line:
                     if (word == ''):
                         continue
-                    if (edge_format == "UPPER_DIAG_ROW"):
+                    if (edge_format == "UPPER_DIAG_ROW"): #trata da matriz upper diagonal
                         if (int(word) == 0):
                             y += 1
                         matrix[x][y] = int(word)
@@ -68,7 +68,7 @@ def readTSP(selectedTSP):
                         if (x == dimension):
                             contador_inicial_x += 1
                             x = contador_inicial_x
-                    else:
+                    else: # lower diagonal
                         matrix[matrixLine][matrixColumn] = int(word)
                         matrix[matrixColumn][matrixLine] = int(word)
                         matrixLine = matrixLine + 1
@@ -82,7 +82,7 @@ def readTSP(selectedTSP):
     return matrix, dimension
 
 
-def writeTSP(currentWorkingDirectory, selectedTSP, cost, route) :
+def writeTSP(currentWorkingDirectory, selectedTSP, cost, route) : #escreve em um novo arquivo a melhor solucao e sua distancia
 
     splittedTSP = selectedTSP.split('/')
     cost = str(cost) + '\n'
@@ -145,11 +145,11 @@ def create_neighbourhood ( initial_solution ) : # gera a vizinhanca de solucoes 
             break
         temporary_solution = initial_solution[:]
         new_neighbour = swap ( temporary_solution, number , number + 1 )
-        neighbours.append(new_neighbour)
+        neighbours.append ( new_neighbour )
 
     return neighbours
 
-def create_initial_solution ( dimension ):
+def create_initial_solution ( dimension ): #cria uma solucao inicial aleatoria
 
     initial_solution = []
 
@@ -181,7 +181,7 @@ def altered_hill_climbing ( matrix , dimension ): #hill climbing que exige um nu
     best_solution = []
     shortest_distance = 0
 
-    while iterations < 10 :
+    while iterations < 1000 :
         if shortest_distance == 0 :
             ( best_solution , shortest_distance ) = hill_climbing ( matrix , dimension )
         else :
@@ -206,3 +206,4 @@ print best_neighbor, distance
 print best_neighbor, distance
 
 writeTSP(cwd, selectedTSP, distance, best_neighbor)
+
