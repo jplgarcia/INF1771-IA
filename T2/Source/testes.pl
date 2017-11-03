ed094858ffeca50dec870c63aadf2cdca73d8934
Rodrigo Leite <rodrigo.ldasilva@hotmail.com>
2017-11-03 13:58:22 -0200

    Implementa turn()
    turn() tem um argumento que pode ter o valor
    r/right/d/direita/c/clockwise
    ou
    l/left/e/esquerda/cc/counter_clockwise

--- T2/Source/testes.pl
+++ T2/Source/testes.pl
@@ -1,6 +1,5 @@
 :- dynamic position/2.
 :- dynamic facing/2.
-
 position(1,1).
 facing(1,0).
 
@@ -12,3 +11,41 @@ step() :-
 	retract(position(_,_)),
 	assert(position(X,Y)),
 	format("position(~a,~a)",[X,Y]).
+	
+
+turn(right) :-
+	facing(Xunit,Yunit),
+	X is Yunit,
+	Y is -Xunit,
+	retract(facing(_,_)),
+	assert(facing(X,Y)),
+	format("facing(~a,~a)",[X,Y]).
+	
+turn(left) :-
+	facing(Xunit,Yunit),
+	X is -Yunit,
+	Y is Xunit,
+	retract(facing(_,_)),
+	assert(facing(X,Y)),
+	format("facing(~a,~a)",[X,Y]).
+
+/turn(X) é para aceitar outras nomenclaturas para left e right/
+turn(X) :-
+	(
+		(
+			X==l;
+			X==e;
+			X==esquerda;
+			X==cc;
+			X==counter_clockwise),/cc = "counter clockwise"/
+		turn(left),!/"!" é para não continuar a avaliação (como um return)/
+	);
+	(
+		(
+			X==r;
+			X==d;
+			X==direita;
+			X==c;/c = "clockwise"/
+			X==clockwise),
+		turn(right),!
+	).
