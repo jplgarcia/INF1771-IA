d6763dfddcfc6abe3172b4398959a23f44149f50
juliaaleixo <julia.pereira1995@outlook.com>
2017-11-09 23:27:09 -0200

    pontuacoes
    ta bonitinho

--- T2/Source/view.py
+++ T2/Source/view.py
@@ -4,33 +4,8 @@ import time
 
 master = Tk()
 
-faceCanvas = Canvas ( master, width = 80, height = 80  )
-imageWWFace = ImageTk.PhotoImage(Image.open("WWface.jpg"))
-WWfacesprite =  faceCanvas.create_image ( 45, 45, image = imageWWFace)
-faceCanvas.grid ( row = 0, column = 0, rowspan = 3 )
-
-#cria secao para pontuacoes, energia e flecha
-labelPoints = Label ( master, text = "Pontos : " )
-labelNumPoints = Label ( master, text = "0" )
-
-labelPoints.grid ( row = 0 , column = 1 )
-labelNumPoints.grid ( row = 0, column = 2 )
-
-labelEnergy = Label ( master, text = "Energia : " )
-labelNumEnergy = Label ( master, text = "100" )
-labelEnergy.grid ( row = 1 , column = 1 )
-labelNumEnergy.grid ( row = 1, column = 2 )
-
-labelAmmo = Label ( master, text = "Flechas : " )
-labelNumAmmo = Label ( master, text = "5" )
-labelAmmo.grid ( row = 2 , column = 1 )
-labelNumAmmo.grid ( row = 2, column = 2 )
-
-
-boardCanvas = Canvas ( master, width = 500, height = 600 ) #tamanho do tabuleiro
-rectSize = 40 #cada casa do tabuleiro tem 40 x 40 px
-
 #carrega todas as imagens usadas no tabuleiro
+imageWWFace = ImageTk.PhotoImage(Image.open("WWface.jpg"))
 imageWW = ImageTk.PhotoImage(Image.open("wwleste.gif"))
 imageHole = ImageTk.PhotoImage(Image.open("buraco.png"))
 imageFloor = ImageTk.PhotoImage(Image.open("chao.png"))
@@ -38,27 +13,101 @@ imageGold = ImageTk.PhotoImage(Image.open("ouro.gif"))
 imageMonster20 = ImageTk.PhotoImage(Image.open("monstro20.gif"))
 imageMonster50 = ImageTk.PhotoImage(Image.open("monstro50.gif"))
 
+#insere rosto da agente para inserir ao lado da sua pontuacao
+faceCanvas = Canvas ( master, width = 80, height = 80  )
+WWfacesprite =  faceCanvas.create_image ( 45, 45, image = imageWWFace )
+faceCanvas.grid ( row = 0, column = 0, rowspan = 6 )
+
+#cria secao para pontuacoes, energia e flechas
+labelPoints = Label ( master, text = "Pontos : " )
+labelNumPoints = Label ( master, text = "0" )
+
+labelPoints.grid ( row = 2 , column = 1, sticky = "W" )
+labelNumPoints.grid ( row = 2, column = 2, sticky = "W" )
+
+labelEnergy = Label ( master, text = "Energia : " )
+labelNumEnergy = Label ( master, text = "100" )
+labelEnergy.grid ( row = 3 , column = 1, sticky ="W" )
+labelNumEnergy.grid ( row = 3, column = 2, sticky ="W" )
+
+labelAmmo = Label ( master, text = "Flechas : " )
+labelNumAmmo = Label ( master, text = "5" )
+labelAmmo.grid ( row = 4 , column = 1, sticky = "W" )
+labelNumAmmo.grid ( row = 4, column = 2, sticky = "W" )
+
+#Energia do Monstro 1 (dano 20)
+labelM1 = Label ( master, text = "Monstro 1 : ")
+labelM1Energy = Label ( master, text = "Energia : " )
+labelM1NumEnergy = Label ( master, text = "100" )
+labelM1Damage = Label ( master, text = "Dano : " )
+labelM1NumDamage = Label ( master, text = "20" )
+labelM1.grid( row = 0, column = 3, columnspan = 2, sticky = "W" )
+labelM1Energy.grid( row = 1, column = 3, sticky = "W" )
+labelM1NumEnergy.grid( row = 1, column = 4, sticky = "W" )
+labelM1Damage.grid( row = 2, column = 3, sticky = "W" )
+labelM1NumDamage.grid( row = 2, column = 4, sticky = "W" )
+
+#Energia do Monstro 2 (dano 20)
+labelM2 = Label ( master, text = "Monstro 2 : ")
+labelM2Energy = Label ( master, text = "Energia : " )
+labelM2NumEnergy = Label ( master, text = "100" )
+labelM2Damage = Label ( master, text = "Dano : " )
+labelM2NumDamage = Label ( master, text = "20" )
+labelM2.grid( row = 0, column = 5, columnspan = 2, sticky = "W" )
+labelM2Energy.grid( row = 1, column = 5, sticky = "W" )
+labelM2NumEnergy.grid( row = 1, column = 6, sticky = "W" )
+labelM2Damage.grid( row = 2, column = 5, sticky = "W" )
+labelM2NumDamage.grid( row = 2, column = 6, sticky = "W" )
+
+#Energia do Monstro 3 (dano 50)
+labelM3 = Label ( master, text = "Monstro 3 : ")
+labelM3Energy = Label ( master, text = "Energia : " )
+labelM3NumEnergy = Label ( master, text = "100" )
+labelM3Damage = Label ( master, text = "Dano : " )
+labelM3NumDamage = Label ( master, text = "50" )
+labelM3.grid( row = 3, column = 3, columnspan = 2 , rowspan=3, sticky = "W" )
+labelM3Energy.grid( row = 5, column = 3, sticky = "W" )
+labelM3NumEnergy.grid( row = 5, column = 4, sticky = "W" )
+labelM3Damage.grid( row = 6, column = 3, sticky = "W" )
+labelM3NumDamage.grid( row = 6, column = 4, sticky = "W" )
+
+#Energia do Monstro 4 (dano 50)
+labelM4 = Label ( master, text = "Monstro 4 : ")
+labelM4Energy = Label ( master, text = "Energia : " )
+labelM4NumEnergy = Label ( master, text = "100" )
+labelM4Damage = Label ( master, text = "Dano : " )
+labelM4NumDamage = Label ( master, text = "50" )
+labelM4.grid( row = 3, column = 5, columnspan = 2 , rowspan=3, sticky = "W" )
+labelM4Energy.grid( row = 5, column = 5 , sticky = "W" )
+labelM4NumEnergy.grid( row = 5, column = 6 , sticky = "W" )
+labelM4Damage.grid( row = 6, column = 5 , sticky = "W" )
+labelM4NumDamage.grid( row = 6, column = 6 , sticky = "W" )
+
+boardCanvas = Canvas ( master, width = 500, height = 550 ) #tamanho do tabuleiro
+rectSize = 40 #cada casa do tabuleiro tem 40 x 40 px
+
+
 def insertMonsters ( monsterList ) : #insere os monstros em suas respectivas posicoes no tabuleiro
 
     for monster in monsterList:
         ( x , y , damage ) = monster #(x,y): coordenadas de cada monstro, damage indica se o monstro tem dano de 20 ou 50
         if damage == 20:
-            imagesprite = boardCanvas.create_image (30 + rectSize * (x - 1), 70 + rectSize * (12 - (y - 1)), image = imageMonster20)
+            imagesprite = boardCanvas.create_image ( 30 + rectSize * (x - 1), 20 + rectSize * (12 - (y - 1)), image = imageMonster20 )
         else:
-            imagesprite = boardCanvas.create_image (30 + rectSize * (x - 1), 70 + rectSize * (12 - (y - 1)), image = imageMonster50)
+            imagesprite = boardCanvas.create_image ( 30 + rectSize * (x - 1), 20 + rectSize * (12 - (y - 1)), image = imageMonster50 )
 
 def insertHoles ( holeList ) : #funcao que insere buracos no tabuleiro baseado nas casas sorteadas
 
     for hole in holeList:
         ( x , y ) = hole
         # posicoes no tabuleiro sao de 0 a 11, e as recebidas de 1 a 12
-        imagesprite = boardCanvas.create_image (30 + rectSize * (x - 1), 70 + rectSize * (12 - (y - 1)), image = imageHole)
+        imagesprite = boardCanvas.create_image ( 30 + rectSize * (x - 1), 20 + rectSize * (12 - (y - 1)), image = imageHole )
 
 def insertGold ( goldList ) : #insere ouros no tabuleiro
 
     for gold in goldList :
         (x,y) = gold #lista contem posicao x,y de cada ouro
-        imagesprite = boardCanvas.create_image (30 + rectSize * (x - 1), 70 + rectSize * (12 - (y - 1)), image = imageGold)
+        imagesprite = boardCanvas.create_image ( 30 + rectSize * (x - 1), 20 + rectSize * (12 - (y - 1)), image = imageGold )
 
 def drawLines () : # desenha as linhas do tabuleiro
 
@@ -66,8 +115,8 @@ def drawLines () : # desenha as linhas do tabuleiro
     while i < 12: # i e j sao as linhas e colunas do tabuleiro, que tem dimensao de 12 x 12
         j = 0
         while j < 12:
-            boardCanvas.create_rectangle (10 + rectSize * j, 50 + rectSize * (12 - i), 10 + rectSize * j + rectSize,
-                                          50 + rectSize * (12 - i) + rectSize, fill="", outline='black')
+            boardCanvas.create_rectangle (10 + rectSize * j,  rectSize * (12 - i), 10 + rectSize * j + rectSize,
+                                           rectSize * (12 - i) + rectSize, fill="", outline='black')
             j = j + 1
         i = i + 1
 
@@ -78,7 +127,7 @@ def drawFloor () : #desenha o chao do labirinto/tabuleiro
         j = 0
         while j < 12:
             # insere imagem do chao no tabuleiro em cada casa
-            imagesprite = boardCanvas.create_image ( 30 + rectSize * j, 70 + rectSize * (12 - i), image = imageFloor )
+            imagesprite = boardCanvas.create_image ( 30 + rectSize * j, 20 + rectSize * (12 - i), image = imageFloor )
             j = j + 1
         i = i + 1
 
@@ -99,9 +148,9 @@ def changeDirection ( direction, position ) : #muda direcao da agente em sua res
         imageWW = ImageTk.PhotoImage(Image.open("wwoeste.gif"))
 
     # posiciona imagem com a direcao correta na casa onde a agente se encontra.
-    agentSprite = boardCanvas.create_image(30 + rectSize * (x - 1), 70 + rectSize * (12 - (y - 1)), image = imageWW)
+    agentSprite = boardCanvas.create_image(30 + rectSize * (x - 1), 20 + rectSize * (12 - (y - 1)), image = imageWW)
 
-def movement () :
+def movement () : #executa os movimentos da agente
     #acessar o arquivo wumpus.py que acessa o prolog para determinar movimentos
     time.sleep(0.5)
     changeDirection("East",(2,1))
@@ -131,16 +180,16 @@ def movement () :
 
 
 drawFloor()
-insertHoles([(1,3),(3,3)])
+insertHoles([(1,3),(3,3),(1,12),(2,5),(7,11),(7,8),(4,4),(7,7)])
 insertGold([(2,7),(5,6),(10,9)])
-insertMonsters([(2,2,20),(10,10,50)])
-agentSprite = boardCanvas.create_image(30, 70 + rectSize * 12, image = imageWW)
+insertMonsters([(2,2,20), (3,10,20),(10,10,50),(5,5,50)])
+agentSprite = boardCanvas.create_image ( 30, 20 + rectSize * 12, image = imageWW )
 drawLines()
-boardCanvas.grid ( columnspan = 5 )
+boardCanvas.grid ( columnspan = 8 )
 
-
+#butao para iniciar a simulacao
 button = Button ( master, text = "Start", command = lambda: movement() )
-button.grid ( columnspan = 5, pady = (0,40) )
+button.grid ( columnspan = 8, pady = ( 0, 20 ) )
 
 
 master.mainloop()
