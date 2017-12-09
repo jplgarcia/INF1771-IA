858c7677b50129aea606df7d011d173e808fa819
Rodrigo Leite <rodrigo.ldasilva@hotmail.com>
2017-12-09 11:45:30 -0200

    Redes Neurais

--- T3/neural_network.py
+++ T3/neural_network.py
@@ -1,32 +1,28 @@
-from sklearn import tree
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
-from scipy import  io
-
-#treino = io.mmread("train.mtx")
-#teste = io.mmread("test.mtx")
-
 Xtreino = pd.read_csv('treino.csv')
 Xteste = pd.read_csv('teste.csv')
 
-#print Xtreino.head()
 countvec = CountVectorizer(stop_words = 'english', ngram_range = (1,2), min_df = 2)
 
 treino = countvec.fit_transform(Xtreino.Reviews) # Gera bag of words com palavras que ele nao viu antes
 
 teste = countvec.transform(Xteste.Reviews)
 
-
-file = open("resultados_decision_tree.txt", "a")
-clf = tree.DecisionTreeClassifier()
+hls = (10,7,7,4)
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=hls, random_state=1)
 clf = clf.fit(treino, Xtreino.Positive)
+
+file = open("resultados_neural_network.txt", "a")
+
 
 ypred = clf.predict(teste)
 
 acc = accuracy_score(Xteste.Positive, ypred)
 acc = str(acc)
 print "Resultado: - " + acc
-resultado = "\nDecision Tree: - " + acc
+resultado = "\nNeural Network: - " + acc + " hidden_layer_sizes=" + str(hls)
 file.write(resultado)
 file.close()
